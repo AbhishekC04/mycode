d6063a510bf25672180f864f0c7b10c37c49553d
AbhishekC04 <abhishekchakraborty2@gmail.com>
2021-05-10 19:36:38 +0000

    mixed lists

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+ipv4_addr = input("Please enter an IPv4 IP address: ")
+print("You told me the IPv4 address is: " + ipv4_addr)
+
+vendor_name = input("Enter the vendor name: ")
+print("Your vendor name  is: " vendor_name)
+
+
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# create a list containing three items
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+# display the first item in the list
+print("The SIP socket is : " + my_list[0],":"+str(my_list[1]))
+# display the third item in the list
+print("The last item in the list (state): " + my_list[2] )
+
