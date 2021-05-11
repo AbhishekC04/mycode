7970866028da80c50f4fe721b08dbc7a50732b36
AbhishekC04 <abhishekchakraborty2@gmail.com>
2021-05-11 13:53:00 +0000

    /mycode

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+# display only the IP addresses to the screen.
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
-# create a list containing three items
-my_list = [ "192.168.0.5", 5060, "UP" ]
+# example 1 - add up the strings
+print("IP addresses: " + iplist[3] + ", and " + iplist[4])
 
-# display the first item in the list
-print("The SIP socket is : " + my_list[0],":"+str(my_list[1]))
-# display the third item in the list
-print("The last item in the list (state): " + my_list[2] )
+# example 2 - use the comma separator 
+print("IP addresses:", iplist[3], ", and", iplist[4])
+
+# example 3 - use an 'f-string'
+print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
 
