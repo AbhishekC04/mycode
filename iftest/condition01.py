32c04edee2e050aa0ba84f292f816ae7d3435d8c
AbhishekC04 <abhishekchakraborty2@gmail.com>
2021-05-11 18:38:46 +0000

    committing the if and move

---
+++ iftest/condition01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3`
+
+# create the string hostname
+hostname = "MTG"
+# test logic with the `if` statement
+# what to do if this statement is found to be true
+if hostname == "MTG":
+    print("MTG works fine")
+    print("mtg works fine")
+#!/usr/bin/env python3
+hostname = input("What value should we set for hostname?")
+if hostname == "mtg":
+    print("mtg works fine")
+
