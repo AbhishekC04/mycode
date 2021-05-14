c53b5ad29a94ff7fe5aed12c04f5ce47451e91f7
AbhishekC04 <abhishekchakraborty2@gmail.com>
2021-05-14 00:18:26 +0000

    python and excel, including working with pandas

---
+++ pandas/example.py
@@ -0,0 +1,9 @@
+moviefile = open("movies.json","w")
+    print(blah, file=movies)
+moviefile.close()i
+
+# Does this get you started?
+SUre it does
+Let me try this out in some time, thank you
+# You bet, go get 'em!!!!
+
