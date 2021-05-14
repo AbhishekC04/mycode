151cbe89ccb0d8d305f8342c7236474f14b9fa74
AbhishekC04 <abhishekchakraborty2@gmail.com>
2021-05-14 14:49:19 +0000

    Python and APIs

---
+++ catfacts/facts02.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Alta3 Research | Author: RZFeeser@alta3.com"""
+
+# imports always go at the top of your code
+import requests
+import random
+import crayons
+def main():
+    """Run time code"""
+    ## create r, which is our request object
+    r = requests.get('https://cat-fact.herokuapp.com/facts')
+
+    ## catfact is our iterable -- that just means it will take on the values found within
+    ## r.json()["all"], one after the next-- which happens to be a dictionary
+    ## the code within the loop, says, "from that single dictionary
+    ## print the value associated with text"
+    ##for catfact in r.json():
+        ##print(catfact.get("text"))  # the .get() method returns NONE if key not found
+       
+
+    with open("file.txt", "r") as f:
+        lines = f.readlines()
+        print (crayons.yellow("Below is the random cat text:"))
+        print (random.choice(lines))
+
+main()
+
